fc1202107f264c259d385400155c28683d9a4c15
Alexandr Krylov <alexandr.krylov@gmail.com>
2019-06-14 23:56:51 +0600

    study variables

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
-    println!("Hello, world!");
+    println!("MAX_POINTS is: {}", MAX_POINTS);
+    const MAX_POINTS: u32 = 100_000;
+    print();
+    let x = 5;
+    let x = x + 1;
+    let x = x * 2;
+    println!("The value of x is: {}", x);
+    let spaces = "     ";
+    let spaces = spaces.len();
+    println!("The value of spaces is: {}", spaces);
 }
+
+fn print() {
+    println!("MAX_POINTS is: {}", MAX_POINTS);
+}
+
+const MAX_POINTS: u32 = 200_000;

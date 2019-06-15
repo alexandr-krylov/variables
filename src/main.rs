ed299f68718dba0d6ffa9cab0470ae947cb187a7
Alexandr Krylov <alexandr.krylov@gmail.com>
2019-06-15 17:13:13 +0600

    study types in progress

--- src/main.rs
+++ src/main.rs
@@ -9,6 +9,24 @@ fn main() {
     let spaces = "     ";
     let spaces = spaces.len();
     println!("The value of spaces is: {}", spaces);
+
+    let value = 98_222_000;
+    println!("value = {}", value);
+
+    let value = 0xff;
+    println!("value = {}", value);
+
+    let value = 0o77;
+    println!("value = {}", value);
+
+    let value = 0b1111_0000;
+    println!("value = {}", value);
+
+    let value = b'A';
+    println!("value = {}", value);
+
+    let value:i64 = 9223372036854775807;
+    println!("value = {}", value);
 }
 
 fn print() {
